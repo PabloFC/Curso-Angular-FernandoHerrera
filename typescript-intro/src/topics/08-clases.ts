fd400330f762de88a2465dd08e894c6ed3fad862
PabloFC <pablofc_68@hotmail.com>
2026-01-27 14:53:52 +0100

    extender una clase

--- typescript-intro/src/topics/08-clases.ts
+++ typescript-intro/src/topics/08-clases.ts
@@ -5,12 +5,22 @@ export class Person {
 
   constructor(
     public name: string,
-    private address: string,
+    private address: string = "No address",
   ) {
     // this.name = name;
     // this.address = address;
   }
 }
 
-const ironman = new Person("iroman", "new york");
+export class Hero extends Person {
+  constructor(
+    public alterEgo: string,
+    public age: number,
+    public realName: string,
+  ) {
+    super(realName, "New York");
+  }
+}
+
+const ironman = new Hero("Ironman", 45, "Tony Stark");
 console.log(ironman);
