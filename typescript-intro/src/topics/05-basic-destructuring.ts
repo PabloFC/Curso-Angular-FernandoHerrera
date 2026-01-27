f5922f6145e94f6baecbbceb6f975318164e925a
PabloFC <pablofc_68@hotmail.com>
2026-01-27 13:01:41 +0100

    17 desestructuracion de objetos

---
+++ typescript-intro/src/topics/05-basic-destructuring.ts
@@ -0,0 +1,31 @@
+interface AudioPlayer {
+  AudioVolume: number;
+  songDuration: number;
+  song: string;
+  details: Details;
+}
+
+interface Details {
+  author: string;
+  year: number;
+}
+
+const audioPlayer: AudioPlayer = {
+  AudioVolume: 90,
+  songDuration: 36,
+  song: "Mess",
+  details: {
+    author: "Ed Sheeran",
+    year: 2015,
+  },
+};
+
+const { AudioVolume, songDuration: duration, song, details } = audioPlayer;
+const { author } = details;
+
+console.log("Audio Volume:", AudioVolume);
+console.log("Song Duration:", duration);
+console.log("Song:", song);
+console.log("Author:", author);
+
+export {};
