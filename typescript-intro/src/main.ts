f37b2ac09361fadc345347c111603848be1ef674
PabloFC <pablofc_68@hotmail.com>
2026-01-28 13:22:11 +0100

    encadenamiento

--- typescript-intro/src/main.ts
+++ typescript-intro/src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 // import "./topics/01-basic-types";
 // import "./topics/02-object-interface";
 // import "./topics/03-functions";
-import "./topics/09-generics";
+import "./topics/11-optional-chaining";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = "Hola mundo";
