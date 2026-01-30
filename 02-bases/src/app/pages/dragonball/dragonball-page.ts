0665dbda41dcf3517acd249a43169f4fc4f3f98e
PabloFC <pablofc_68@hotmail.com>
2026-01-30 14:37:20 +0100

    dragonball componente

---
+++ 02-bases/src/app/pages/dragonball/dragonball-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragonball',
+  imports: [],
+  templateUrl: './dragonball-page.html',
+})
+export class DragonballPageComponent {}
