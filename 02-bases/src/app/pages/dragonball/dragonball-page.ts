aaf844e5ae0cf1c0003db7c98cb2de7710405c3c
PabloFC <pablofc_68@hotmail.com>
2026-01-30 14:53:40 +0100

    51. iteraciones

--- 02-bases/src/app/pages/dragonball/dragonball-page.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball',
   imports: [],
   templateUrl: './dragonball-page.html',
 })
-export class DragonballPageComponent {}
+export class DragonballPageComponent {
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 3, name: 'Picolo', power: 3000 },
+  ]);
+}
