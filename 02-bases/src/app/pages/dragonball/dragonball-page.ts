c719bb4cba4b72ffdbbd4e825b17771290feee6c
PabloFC <pablofc_68@hotmail.com>
2026-02-01 00:30:22 +0100

    55 solucion al ejercicio

--- 02-bases/src/app/pages/dragonball/dragonball-page.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.ts
@@ -13,10 +13,35 @@ interface Character {
   templateUrl: './dragonball-page.html',
 })
 export class DragonballPageComponent {
+  name = signal('Gohan');
+  power = signal(100);
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
-    { id: 3, name: 'Picolo', power: 3000 },
     { id: 4, name: 'Yamcha', power: 499 },
+    { id: 3, name: 'Picolo', power: 3000 },
   ]);
+
+  addCharacter() {
+    console.log(this.name(), this.power());
+
+    if (!this.name() || !this.power() || this.power() < 0) {
+      return;
+    }
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    // this.characters().push(newCharacter);
+    this.characters.update((characters) => [...characters, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
