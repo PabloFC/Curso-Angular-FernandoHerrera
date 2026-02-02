25aa62abf68ee3680bc08c9714f3ceef8153bc0d
PabloFC <pablofc_68@hotmail.com>
2026-02-02 21:27:13 +0100

    environments

--- 03-gifs-app/src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'side-menu-header',
   templateUrl: './side-menu-header.html',
 })
-export class SideMenuHeaderComponent {}
+export class SideMenuHeaderComponent {
+  envs = environment;
+}
