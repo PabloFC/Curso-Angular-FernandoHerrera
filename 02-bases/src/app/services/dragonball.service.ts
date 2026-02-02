63e56670934c75e86f9975051cef3de09a3535ec
PabloFC <pablofc_68@hotmail.com>
2026-02-02 12:55:40 +0100

    localstorage

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({ providedIn: 'root' })
@@ -7,6 +7,11 @@ export class DragonBallService {
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
   ]);
+
+  saveToLocalStorage = effect(() => {
+    // console.log(`Character count ${this.characters().length}`);
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  });
 
   addCharacter(character: Character) {
     this.characters.update((list) => [...list, character]);
