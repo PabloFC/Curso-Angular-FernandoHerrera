df312d2de45084efb8a84a12f22adf58f5887915
PabloFC <pablofc_68@hotmail.com>
2026-02-02 21:08:46 +0100

    72 routerLink

--- 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,7 +1,31 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'side-menu-options',
   templateUrl: './side-menu-options.html',
+  imports: [RouterLink, RouterLinkActive],
 })
-export class SideMenuOptionsComponent {}
+export class SideMenuOptionsComponent {
+  public menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search',
+    },
+  ];
+}
