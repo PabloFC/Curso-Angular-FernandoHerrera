71eb744db71577077161a4af6b8f40c8958480e9
PabloFC <pablofc_68@hotmail.com>
2026-02-03 14:23:26 +0100

    service interfaces

--- 03-gifs-app/src/environments/environment.development.ts
+++ 03-gifs-app/src/environments/environment.development.ts
@@ -3,4 +3,9 @@ export const environment = {
   companyName: 'Gifs',
   companyName2: 'App',
   companySlogan: 'Maneja tus gifs',
+  giphyApiKey: 'cLNzkeSEaVPHWxEgiyzHijdt1cOZ1vMc',
+  giphyUrl: 'https://api.giphy.com/v1',
 };
+
+// URL
+// https://api.giphy.com/v1/clips/trending?api_key=&limit=25&offset=0&country_code=&rating=g
